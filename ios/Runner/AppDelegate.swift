d02d06a41009ec29e3f62904a761581ff58e2a11
sanjay-sharma <sanjaysharmajw@gmail.com>
2023-01-20 19:01:41 +0530

    change in RiderFamilyList

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,12 +8,11 @@ import GoogleMaps
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GMSServices.provideAPIKey("AIzaSyAmOl-QJnc2Spwuh8GAoqx9Z3Wz-ez73V8")
     GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
