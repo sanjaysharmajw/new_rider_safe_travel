961ce8c7d1e714dfb43d9c2e89e07e2c823e66fc
sanjay-sharma <sanjaysharmajw@gmail.com>
2023-02-23 12:56:45 +0530

    Stop Alert is integrated

--- lib/Utils/custom_dialog.dart
+++ lib/Utils/custom_dialog.dart
@@ -1,36 +1,43 @@
 // import 'package:flutter/material.dart';
 // import 'package:overlay_loading_progress/overlay_loading_progress.dart';
 //
-// class ReceivedAlert extends StatelessWidget {
-//   const ReceivedAlert({Key? key}) : super(key: key);
+// import 'exit_alert_dialog.dart';
 //
-//   @override
-//   Widget build(BuildContext context) {
-//     return  showDialog(
+// class CustomDialog{
+//   Future<void> stopAlert(BuildContext context) async {
+//     return showDialog<void>(
 //       context: context,
 //       barrierDismissible: true, // user must tap button!
 //       builder: (BuildContext context) {
 //         return AlertDialog(
-//             title: const Text('Alert'),
-//             content: SingleChildScrollView(
-//               child: ListBody(
-//                 children: const <Widget>[
-//                   Text('Do you received support?'),
-//                 ],
-//               ),
+//           title: const Text('Alert'),
+//           content: SingleChildScrollView(
+//             child: ListBody(
+//               children: const <Widget>[
+//                 Text('Dou you want to stop?'),
+//               ],
 //             ),
-//             actions: <Widget>[
-//         TextButton(
-//         child: const Text('No'),
-//         onPressed: () {
-//         Navigator.pop(context, true);
-//         },
-//         ),
-//         TextButton(
-//         child: const Text('Yes'),
-//         onPressed: () {
-//         });
+//           ),
+//           actions: <Widget>[
+//             TextButton(
+//               child: const Text('No'),
+//               onPressed: () {
+//                 Navigator.pop(context, true);
+//               },
+//             ),
+//             TextButton(
+//               child: const Text('Ok'),
+//               onPressed: () {
+//                 Navigator.pop(context, true);
+//                 showExitPopup(context, "Do you want to stop ride?", () async {
+//                   OverlayLoadingProgress.start(context);
+//                   await endRide();
+//                 });
+//               },
+//             ),
+//           ],
+//         );
 //       },
-//     ),
+//     );
 //   }
 // }
